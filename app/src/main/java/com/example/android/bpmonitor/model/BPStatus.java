9813b68fec89b5e9349a0952500cc4406ee7e7da
Peter Corletto <peter.corletto@gmail.com>
2016-01-31 10:27:20 -0500

    26th commit: Fixed the shifting to the right when list view of weekly readings is displayed.

--- app/src/main/java/com/example/android/bpmonitor/model/BPStatus.java
+++ app/src/main/java/com/example/android/bpmonitor/model/BPStatus.java
@@ -10,22 +10,26 @@ public class BPStatus{
     public String getBPStatus(double s, double d){
         if((s>120)||(d>80)){
 
-            mBPStatus = "high        ";
+            mBPStatus = "high       ";
 
             if(s<90){
-                mBPStatus = "S:LO, D:HI ";
+
+                mBPStatus = "S:LO, D:HI " ;
             }
 
             if(d<60){
-                mBPStatus = "S:HI, D:LO ";
+
+                mBPStatus = "S:HI, D:LO " ;
             }
 
         }
         else if (((s>=90)&&(s<=120)&&((d>=60)&&(d<=80)))) {
-            mBPStatus = "normal      ";
+
+            mBPStatus = "normal     ";
         }
         else{
-            mBPStatus = "low         ";
+             
+            mBPStatus = "low        ";
         }
         return mBPStatus;
     }
--- app/src/main/java/com/example/android/bpmonitor/ui/MainActivity.java
+++ app/src/main/java/com/example/android/bpmonitor/ui/MainActivity.java
@@ -393,7 +393,7 @@ public class MainActivity extends ActionBarActivity {
 
 
 
-                
+
 
 
 
